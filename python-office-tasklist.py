28cdc3a5bbc8c62b1bacb345c22772a4d27c7e42
Yasemin <yacey@hotmail.de>
2024-10-04 17:18:59 +0200

    end

--- python-office-tasklist.py
+++ python-office-tasklist.py
@@ -9,10 +9,28 @@ def add_task():                #define add function
         print("Your tasklist is emty.")
 add_task()
 
+def show_tasklist():                #define show tasklist fuction
+    if office_tasklist == None:     # check if tasklist is empty
+        print("Your tasklist is empty.") 
+    else:
+        for task in office_tasklist: #entre a for - loop to print the input of the user
+            print("Your tasklist:")
+            print(task)
 
-# def main():                             #define a main function
- # while True:
- #if
- 
-faelligkeitsdatum = input("Bitte geben Sie das Fälligkeitsdatum im Format TT.MM.JJJJ ein: ")
-print("Das eingegebene Fälligkeitsdatum ist:", faelligkeitsdatum)
+
+def main():                             # define main function
+    while True:                         # while true do loop
+        print("1. Add a task.")
+        print(" Show the list.")
+        print(" End the programm.")
+        choice = input("Please make your choice") #ask a user to make a choice
+        if choice == "1":
+            add_task()
+        elif choice =="2":
+            show_tasklist()
+        elif choice == "3":
+            print("Goodbye.")   
+            break                       # break to end the while - loop
+if __name__ == "main":                    # control function
+    main()
+
